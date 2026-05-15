aa28fd2e2d78e69997d9f1bb8e01fe2b6e73ca0e
Benjamin Eberlei <kontakt@beberlei.de>
2026-05-15 11:18:12 +0200

    Fixes to Apache

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
-REPO_FOLDER="/workspaces/$RepositoryName"
+REPO_FOLDER="$PWD"
 
 # Apache
-sudo chmod 777 /etc/apache2/sites-available/000-default.conf
-sudo sed "s@.*DocumentRoot.*@\tDocumentRoot $PWD/wordpress@" .devcontainer/000-default.conf >/etc/apache2/sites-available/000-default.conf
-update-rc.d apache2 defaults
-service apache2 start
+sudo sed "s@%%DOCROOT%%@$PWD/wordpress@g" .devcontainer/000-default.conf | sudo tee /etc/apache2/sites-available/000-default.conf >/dev/null
+sudo a2enmod rewrite
+sudo service apache2 start
 
 LOCALE="de_DE"
 echo memory_limit=256M | sudo tee -a /usr/local/etc/php/conf.d/xmemory.ini
@@ -16,7 +15,7 @@ cd wordpress
 wp config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=db
 LINE_NUMBER=$(grep -n -o 'stop editing!' wp-config.php | cut -d ':' -f 1)
 sed -i "${LINE_NUMBER}r ../.devcontainer/wp-config-addendum.txt" wp-config.php && sed -i -e "s/CODESPACE_NAME/$CODESPACE_NAME/g" wp-config.php
-wp core install --url=https://$(CODESPACE_NAME) --title=WordPress --admin_user=admin --admin_password=admin --admin_email=mail@example.com
+wp core install --url=https://${CODESPACE_NAME}-80.app.github.dev --title=WordPress --admin_user=admin --admin_password=admin --admin_email=mail@example.com
 
 # Selected plugins
 wp plugin delete akismet
